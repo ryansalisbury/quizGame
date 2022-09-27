5eca180c287d1c7e66ef739e588b4c8d57c2e1b3
ryansalisbury <ryansalisbury10@icloud.com>
2022-09-27 13:57:43 +0100

    Unfinished js document - research this keyword

---
+++ myscript.js
@@ -0,0 +1,40 @@
+/**Remember that questions is an object as there are {}
+ * when there is [] it is an array!
+ */
+
+let questions =[{
+    question: "what is the baby of a moth know as?",
+    choices: ["baby","infant", "kit", "larva"],
+    correctAnswer: 3
+},{
+    question: "what is the adult of a kid know as?",
+    choices: ["baby","infant", "goat", "larva"],
+    correctAnswer: 2
+},{
+    question: "what is the young of buffalo know as?",
+    choices: ["calf","infant", "kit", "larva"],
+    correctAnswer: 0
+},{
+    question: "what is a baby aligator know as?",
+    choices: ["baby","infant", "hatchling", "larva"],
+    correctAnswer: 2
+},{
+    question: "what is a baby goose know as?",
+    choices: ["baby","gosling", "kit", "larva"],
+    correctAnswer: 1
+}]
+
+let currentQuestion = 0;
+let correctAnswers = 0;
+let quizOver = false;
+
+/**This function just checks that the page is fully loaded before executing anything */
+$(document).ready(function()){
+
+    /**the keyword 'this' represents the object that 
+     * is executing the current function
+     */
+
+    return 0;
+
+}
